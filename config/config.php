4d80490de0c6dfde4dc145e33da6de74d567789c
Nookyava <jcw21396@gmail.com>
2015-02-04 23:45:42 -0500

    Initial Commit
    Initial Commit

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?
+	$apikey = ''; // Your API key
+	$region = 'na'; // What regions are you? North Amerca?
+	
+	return array(
+		'region' => $region,
+		'key' => $apikey,
+	);
+?>
---
+++ functions.php
@@ -0,0 +1,89 @@
+<?php	
+	/*
+		Name: requestSummonerInfo
+		Arguments: name (string)
+		Returns: result (array)
+		Desc: Gets data about the summoner like their level, avatar and name
+	*/
+	function requestSummonerInfo($name) {
+		$config = include('config/config.php'); // Include the config
+		$url = 'https://na.api.pvp.net/api/lol/'.$config['region'].'/v1.4/summoner/by-name/'.$name.'?api_key='.$config['key'];
+		
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //this will return the content as a string instead of printing it out instantly $result = curl_exec($ch);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		
+		$summonerdata = json_decode($result, true); //This will put the json data in an associate array for quick access later.
+		return $summonerdata;
+	}
+	
+	/* 
+		Name: getSummonerStats
+		Arguments: id (string),
+		Returns: stats (array)
+		Desc: Returns how many times the player has won on that map
+	*/
+	function getSummonerStats($id) {
+		$config = include('config/config.php'); // Include the config
+		$url = 'https://na.api.pvp.net/api/lol/'.$config['region'].'/v1.3/stats/by-summoner/'.$id.'/summary/?api_key='.$config['key'];
+		
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		
+		$summonerdata = json_decode($result, true); //This will put the json data in an associate array for quick access later.
+		return $summonerdata;
+	}
+	
+	/* 
+		Name: getSummonerWins
+		Arguments: summonerstats (array)
+		Returns: wins (string)
+		Desc: Returns how many times the player has won on that map
+	*/
+	function getSummonerWins($summonerstats) {
+		return $summonerstats['playerStatSummaries'][9]['wins'];
+	}
+	
+	/*
+		Name: getSummonerID
+		Arguments: name (string), summonerdata (array)
+		Returns: id (string)
+		Desc: Returns the id of the summoner
+	*/
+	function getSummonerID($name, $summonerdata) {
+		return $summonerdata[$name]['id'];
+	}
+	
+	/*
+		Name: getSummonerName
+		Arguments: name (string), summonerdata (array)
+		Returns: level (string)
+		Desc: Returns the level of the summoner
+	*/
+	function getSummonerName($name, $summonerdata) {
+		return $summonerdata[$name]['name'];
+	}
+	
+	/*
+		Name: getSummonerLevel
+		Arguments: name (string), summonerdata (array)
+		Returns: level (string)
+		Desc: Returns the level of the summoner
+	*/
+	function getSummonerLevel($name, $summonerdata) {
+		return $summonerdata[$name]['summonerLevel'];
+	}
+	
+	/*
+		Name: getSummonerAvatar
+		Arguments: name (string), summonerdata(array)
+		Returns: avatarid (string)
+		Desc: Returns the avatar of the player
+	*/
+	function getSummonerAvatar($name) {
+		return $summonerdata[$name]['profileIconId'];
+	}
+?>
