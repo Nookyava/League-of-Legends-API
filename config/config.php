b82d4b9352f0a75ff1146e1de8a1d240da355569
Jacob <jcw21396@gmail.com>
2015-02-04 23:57:23 -0500

    Fixed spelling error

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?
 	$apikey = ''; // Your API key
-	$region = 'na'; // What regions are you? North Amerca?
+	$region = 'na'; // What regions are you? North America?
 	
 	return array(
 		'region' => $region,
