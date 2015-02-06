1c613f57c266386d17974ed2fb9bb87037685e08
Nookyava <jcw21396@gmail.com>
2015-02-06 00:35:12 -0500

    Updated to Class

--- functions.php
+++ functions.php
@@ -1,108 +1,108 @@
-<?php	
-	/*
-		Name: requestSummonerInfo
-		Arguments: name (string)
-		Returns: result (array)
-		Desc: Gets data about the summoner like their level, avatar and name
-	*/
-	function requestSummonerInfo($name) {
-		$config = include('config/config.php'); // Include the config
-		$url = 'https://na.api.pvp.net/api/lol/'.$config['region'].'/v1.4/summoner/by-name/'.$name.'?api_key='.$config['key'];
+<?php
+	class LoLAPI {
+		private $summonerinfo = array();
+		private $summonername;
+		private $summonerid;
 		
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //this will return the content as a string instead of printing it out instantly $result = curl_exec($ch);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		
-		$summonerdata = json_decode($result, true); //This will put the json data in an associate array for quick access later.
-		return $summonerdata;
-	}
-	
-	/* 
-		Name: getSummonerStats
-		Arguments: id (string),
-		Returns: stats (array)
-		Desc: Returns how many times the player has won on that map
-	*/
-	function getSummonerStats($id) {
-		$config = include('config/config.php'); // Include the config
-		$url = 'https://na.api.pvp.net/api/lol/'.$config['region'].'/v1.3/stats/by-summoner/'.$id.'/summary/?api_key='.$config['key'];
-		
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		
-		$summonerdata = json_decode($result, true); //This will put the json data in an associate array for quick access later.
-		return $summonerdata;
-	}
-	
-	/* 
-		Name: getSummonerWins
-		Arguments: summonerstats (array)
-		Returns: wins (string)
-		Desc: Returns how many times the player has won on that map
-	*/
-	function getSummonerWins($summonerstats) {
-		for($i = 0, $size = count($summonerstats['playerStatSummaries']); $i < $size; $i++) {
-			if ($summonerstats['playerStatSummaries'][$i]['playerStatSummaryType'] == 'Unranked') {
-				return $summonerstats['playerStatSummaries'][$i]['wins'];
-				break; // I break here just so I make sure it doesn't return 0. Seems redundant but I'd rather make sure.
-			}
+		/* 
+			Name: setSummonerInfo
+			Arguments: name (string)
+			Returns: nil
+			Desc: Sets all the variables necessary
+		*/
+		public function setSummonerInfo($enteredname) {
+			$this->summonerinfo['general'] = $this->requestSummonerInfo($enteredname);
+			$this->summonerinfo['stats'] = $this->getSummonerStats($this->summonerinfo['general'][$enteredname]['id']);
+			
+			$this->summonername = $enteredname;
+			$this->summonerid = $this->summonerinfo['general'][$enteredname]['id'];
 		}
-		return 0;
-	}
-	
-	/*
-		Name: getSummonerID
-		Arguments: name (string), summonerdata (array)
-		Returns: id (string)
-		Desc: Returns the id of the summoner
-	*/
-	function getSummonerID($name, $summonerdata) {
-		return $summonerdata[$name]['id'];
-	}
-	
-	/*
-		Name: getSummonerName
-		Arguments: name (string), summonerdata (array)
-		Returns: level (string)
-		Desc: Returns the level of the summoner
-	*/
-	function getSummonerName($name, $summonerdata) {
-		return $summonerdata[$name]['name'];
-	}
-	
-	/*
-		Name: getSummonerLevel
-		Arguments: name (string), summonerdata (array)
-		Returns: level (string)
-		Desc: Returns the level of the summoner
-	*/
-	function getSummonerLevel($name, $summonerdata) {
-		return $summonerdata[$name]['summonerLevel'];
-	}
-	
-	/*
-		Name: getSummonerIconID
-		Arguments: name (string), summonerdata (array)
-		Returns: level (string)
-		Desc: Returns the level of the summoner
-	*/
-	function getSummonerIconID($name, $summonerdata) {
-		return $summonerdata[$name]['profileIconId'];
-	}
-	
-	/*
-		Name: getSummonerAvatarPNG
-		Arguments: name (string), summonerdata(array)
-		Returns: avatarid (string)
-		Desc: Returns the avatar of the player
-	*/
-	function getSummonerAvatar($iconid) {
-		$iconver = '5.2.2';
-		$iconurl = 'http://ddragon.leagueoflegends.com/cdn/'.$iconver.'/img/profileicon/'.$iconid.'.png';
 		
-		return $iconurl;
+		/*
+			Name: requestSummonerInfo
+			Arguments: name (string)
+			Returns: result (array)
+			Desc: Gets data about the summoner like their level, avatar and name
+		*/
+		private function requestSummonerInfo($name) {
+			$config = include('config/config.php'); // Include the config
+			$url = 'https://na.api.pvp.net/api/lol/'.$config['region'].'/v1.4/summoner/by-name/'.$name.'?api_key='.$config['key'];
+			
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //this will return the content as a string instead of printing it out instantly $result = curl_exec($ch);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			
+			$summonerdata = json_decode($result, true); //This will put the json data in an associate array for quick access later.
+			return $summonerdata;
+		}
+		
+		/* 
+			Name: getSummonerStats
+			Arguments: id (string),
+			Returns: stats (array)
+			Desc: Returns how many times the player has won on that map
+		*/
+		private function getSummonerStats($id) {
+			$config = include('config/config.php'); // Include the config
+			$url = 'https://na.api.pvp.net/api/lol/'.$config['region'].'/v1.3/stats/by-summoner/'.$id.'/summary/?api_key='.$config['key'];
+			
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			
+			$summonerdata = json_decode($result, true); //This will put the json data in an associate array for quick access later.
+			return $summonerdata;
+		}
+		
+		/*
+			Name: getSummonerName
+			Arguments: name (string), summonerdata (array)
+			Returns: level (string)
+			Desc: Returns the level of the summoner
+		*/
+		public function getSummonerName() {;
+			return $this->summonerinfo['general'][$this->summonername]['name'];
+		}
+		
+		/*
+			Name: getSummonerLevel
+			Arguments: name (string), summonerdata (array)
+			Returns: level (string)
+			Desc: Returns the level of the summoner
+		*/
+		public function getSummonerLevel() {
+			return $this->summonerinfo['general'][$this->summonername]['summonerLevel'];
+		}
+		
+		/*
+			Name: getSummonerAvatarPNG
+			Arguments: name (string), summonerdata(array)
+			Returns: avatarid (string)
+			Desc: Returns the avatar of the player
+		*/
+		public function getSummonerAvatar() {
+			$iconver = '5.2.2';
+			$iconurl = 'http://ddragon.leagueoflegends.com/cdn/'.$iconver.'/img/profileicon/'.$this->summonerinfo['general'][$this->summonername]['profileIconId'].'.png';
+			
+			return $iconurl;
+		}
+		
+		/* 
+			Name: getSummonerWins
+			Arguments: summonerstats (array)
+			Returns: wins (string)
+			Desc: Returns how many times the player has won on that map
+		*/
+		public function getSummonerWins() {
+			for($i = 0, $size = count($this->summonerinfo['stats']['playerStatSummaries']); $i < $size; $i++) {
+				if ($this->summonerinfo['stats']['playerStatSummaries'][$i]['playerStatSummaryType'] == 'Unranked') {
+					return $this->summonerinfo['stats']['playerStatSummaries'][$i]['wins'];
+					break;
+				}
+			}
+			return 0;
+		}
 	}
 ?>
